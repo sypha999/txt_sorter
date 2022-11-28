9f87d40aebe6a15090acdf4b4d56f6f377032aef
Oragbakosi Anthony Arinze <38960714+sypha999@users.noreply.github.com>
2022-11-28 01:55:35 +0100

    Update AppServiceImpl.java

--- src/main/java/com/subsidian/services/serviceImpl/AppServiceImpl.java
+++ src/main/java/com/subsidian/services/serviceImpl/AppServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.SQLOutput;
 import java.util.*;
 
 @Service
